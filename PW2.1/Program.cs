089cb00e9ec7b34864c456f636c595aea4835a9f
Alexey Krix <alexey@thefirstthelast.agency>
2023-02-25 18:51:07 +0200

    Add project files.

---
+++ PW2.1/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace LambdaExpressionsExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Student> students = ReadDataFromFile("students.txt");
+
+            double minGrade = 60.0;
+            double maxGrade = 100.0;
+
+            List<Student> filteredStudents = students
+                .Where(s => s.Grade >= minGrade && s.Grade <= maxGrade)
+                .ToList();
+
+            foreach (Student student in filteredStudents)
+            {
+                Console.WriteLine($"{student.Name} has a grade {student.Grade}");
+            }
+
+            Console.ReadKey();
+        }
+
+        static List<Student> ReadDataFromFile(string filePath)
+        {
+            List<Student> students = new List<Student>();
+
+            try
+            {
+                string[] lines = File.ReadAllLines(filePath);
+
+                foreach (string line in lines)
+                {
+                    string[] parts = line.Split(',');
+                    string name = parts[0];
+                    double grade = double.Parse(parts[1]);
+
+                    students.Add(new Student(name, grade));
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error reading data from file: {ex.Message}");
+            }
+
+            return students;
+        }
+    }
+
+    class Student
+    {
+        public string Name { get; set; }
+        public double Grade { get; set; }
+
+        public Student(string name, double grade)
+        {
+            Name = name;
+            Grade = grade;
+        }
+    }
+}
---
+++ PW2.2/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace LambdaExpressionsExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Employee> employees = ReadDataFromFile("employees.txt");
+
+            List<Employee> sortedBySalary = employees
+                .OrderByDescending(e => e.Salary)
+                .ToList();
+
+            Console.WriteLine("Sorted by salary:");
+            foreach (Employee employee in sortedBySalary)
+            {
+                Console.WriteLine($"{employee.Name} - {employee.Salary:C} | {employee.YearsOfExperience} years");
+            }
+
+            Console.WriteLine();
+
+            List<Employee> sortedByExperience = employees
+                .OrderByDescending(e => e.YearsOfExperience)
+                .ToList();
+
+            Console.WriteLine("Sorted by experience:");
+            foreach (Employee employee in sortedByExperience)
+            {
+                Console.WriteLine($"{employee.Name} - {employee.YearsOfExperience} years of experience | {employee.Salary}$");
+            }
+
+            Console.ReadKey();
+        }
+
+        static List<Employee> ReadDataFromFile(string filePath)
+        {
+            List<Employee> employees = new List<Employee>();
+
+            try
+            {
+                string[] lines = File.ReadAllLines(filePath);
+
+                foreach (string line in lines)
+                {
+                    string[] parts = line.Split(',');
+                    string name = parts[0];
+                    decimal salary = decimal.Parse(parts[1]);
+                    int yearsOfExperience = int.Parse(parts[2]);
+
+                    employees.Add(new Employee(name, salary, yearsOfExperience));
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error reading data from file: {ex.Message}");
+            }
+
+            return employees;
+        }
+    }
+
+    class Employee
+    {
+        public string Name { get; set; }
+        public decimal Salary { get; set; }
+        public int YearsOfExperience { get; set; }
+
+        public Employee(string name, decimal salary, int yearsOfExperience)
+        {
+            Name = name;
+            Salary = salary;
+            YearsOfExperience = yearsOfExperience;
+        }
+    }
+}
