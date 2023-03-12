e88fd79d54b5467e5debeaa065c14f37b2cf86d1
Alexey Krix <alexey@thefirstthelast.agency>
2023-03-12 19:18:44 +0200

    added 3 and 4 task solutions

---
+++ PW2.3/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+
+namespace LambdaGrouping
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string json = File.ReadAllText("data.json");
+
+            Product[] products = JsonSerializer.Deserialize<Product[]>(json);
+
+            var productsByCategory = products.GroupBy(p => p.Category);
+
+            foreach (var group in productsByCategory)
+            {
+                Console.WriteLine("Category: " + group.Key);
+
+                foreach (var product in group)
+                {
+                    Console.WriteLine("- " + product.Name);
+                }
+
+                Console.WriteLine();
+            }
+
+            var productsByPriceRange = products
+                .GroupBy(p =>
+                {
+                    if (p.Price < 50)
+                    {
+                        return "Low (<50)";
+                    }
+                    else if (p.Price < 100)
+                    {
+                        return "Medium (<100)";
+                    }
+                    else
+                    {
+                        return "High (>100)";
+                    }
+                });
+
+            foreach (var group in productsByPriceRange)
+            {
+                Console.WriteLine("Price Range: " + group.Key);
+
+                foreach (var product in group)
+                {
+                    Console.WriteLine("- " + product.Name);
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+    }
+
+    public class Product
+    {
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public decimal Price { get; set; }
+    }
+}
---
+++ PW2.4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MyProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> strings = new List<string> { "This is", "an example", "text", "that can", "be", "to analize", "it legth" };
+
+            Func<string, int> stringLength = s => s.Length;
+
+            List<int> stringLengths = strings.Select(stringLength).ToList();
+
+            Console.WriteLine("Strings:");
+            foreach (string s in strings)
+            {
+                Console.WriteLine($"- {s}");
+            }
+            Console.WriteLine("String Lengths:");
+            foreach (int length in stringLengths)
+            {
+                Console.WriteLine($"- {length}");
+            }
+        }
+    }
+}
